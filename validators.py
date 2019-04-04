5fd20d42a488981d511b0e33a4aa3097a367fe4c
Tomisin Abiodun <so.tomisin@gmail.com>
2019-04-04 10:27:25 +0100

    Refactor code

--- validators.py
+++ validators.py
@@ -2,9 +2,21 @@ import re
 
 
 def validate_password(password):
-    password_pattern = r'^[A-Za-z0-9_\-@#$%]+$'
+    password_pattern = r'^([A-Za-z0-9_\-@#$%]){16,}$'
+    password_pattern_2 = r'^.*\d.*.*\d.*.*\d.*'
+    password_pattern_3 = r'^[A-Z][A-Z].*'
+    password_pattern_4 = r'^.*[-@#$%].*.*[-@#$%].*'
 
-    if len(password) < 16:
-        return False
+    if not re.match(password_pattern, password):
+        raise ValueError('Invalid password')
+
+    if not re.match(password_pattern_2, password):
+        raise ValueError('Invalid password')
+
+    if not re.match(password_pattern_3, password):
+        raise ValueError('Invalid password')
+
+    if not re.match(password_pattern_4, password):
+        raise ValueError('Invalid password')
 
     return True
